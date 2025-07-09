5158ecf9164735b8e71f5d9c09f1958ec1e7daf2
Apsinisde <fzickwolff@googlemail.com>
2025-07-09 15:46:56 +0200

    CH2: Functions | L3: Get File Content

---
+++ functions/get_file_content.py
@@ -0,0 +1,21 @@
+import os
+from functions.config import MAX_CHARS
+
+def get_file_content(working_directory, file_path):
+    try:
+        abs_working_dir = os.path.abspath(working_directory)
+        target_file_path = os.path.join(working_directory, file_path)
+        abs_target_file_path = os.path.abspath(target_file_path)
+        if abs_working_dir not in abs_target_file_path:
+            return(f'Error: Cannot list "{file_path}" as it is outside the permitted working directory')
+        if not os.path.isfile(target_file_path):
+            return (f'Error: "{file_path}" is not a file')
+
+        with open(target_file_path, "r") as f:
+            file_content_string = f.read()
+    except Exception as e:
+        return f"Error: {e}"
+    
+    if len(file_content_string) > MAX_CHARS:
+        file_content_string =  file_content_string[:MAX_CHARS - 1] + f"\n[...File {file_path} truncated at 10000 characters]"
+    return file_content_string
--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,16 +2,15 @@ import os
 
 def get_files_info(working_directory, directory=None):
     try:
-        abs_working_dir = os.path.abspath(working_directory)
-        abs_target_dir = os.path.join(working_directory, directory)
-        if abs_working_dir not in os.path.abspath(abs_target_dir):
+        target_dir = os.path.join(working_directory, directory)
+        if os.path.abspath(working_directory) not in os.path.abspath(target_dir):
             return(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
-        if not os.path.isdir(abs_target_dir):
+        if not os.path.isdir(target_dir):
             return (f'Error: "{directory}" is not a directory')
         
         files_info = []
-        for dir in os.listdir(abs_target_dir):
-            path = os.path.join(abs_target_dir, dir)
+        for dir in os.listdir(target_dir):
+            path = os.path.join(target_dir, dir)
             files_info.append(f"- {dir}: file_size={os.path.getsize(path)}, is_dir={os.path.isdir(path)}")
     except Exception as e:
         return (f'Error: {e}') 
